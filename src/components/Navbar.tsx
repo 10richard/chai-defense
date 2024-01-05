24c6c652e56bbf68f3a1b347a9483d22c1ae69a2
Richard Ten <rten04@icloud.com>
2024-01-04 22:46:03 -0500

    create MaxWidthContainer styled component and finish Navbar structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,28 @@
+import Link from "next/link";
+import { MaxWidthContainer } from "./styled/MaxWidthContainer";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navLinks = [
+    { name: "HOME", href: "/" },
+    { name: "LOCATIONS", href: "/locations" },
+    { name: "ABOUT", href: "/about" },
+    { name: "ATTEND CLASS", href: "/attend-a-class" },
+    { name: "CONTACT US", href: "/contact-us" },
+  ];
+
+  return (
+    <nav className="flex justify-center py-4 px-6 fixed w-full">
+      <MaxWidthContainer>
+        <ul className="font-bold flex justify-between w-full max-w-[645px]">
+          {navLinks.map((navLink, idx) => (
+            <li key={idx}>
+              <Link href={navLink.href}>{navLink.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </MaxWidthContainer>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/components/styled/MaxWidthContainer.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import styled from "styled-components";
+
+export const MaxWidthContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
