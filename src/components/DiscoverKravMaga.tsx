e16309526242fbcba6f5e21bb2675cba76f296d9
Richard Ten <rten04@icloud.com>
2024-01-06 13:20:05 -0500

    create component

---
+++ src/components/DiscoverKravMaga.tsx
@@ -0,0 +1,13 @@
+import { MaxWidthContainer } from "./styled/MaxWidthContainer";
+
+const DiscoverKravMaga = () => {
+  return (
+    <section>
+      <MaxWidthContainer>
+        <div>What is Krav Maga</div>
+      </MaxWidthContainer>
+    </section>
+  );
+};
+
+export default DiscoverKravMaga;
