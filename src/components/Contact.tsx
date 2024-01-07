aee493904ad20a74c72e18353aa7958be179f174
Richard Ten <rten04@icloud.com>
2024-01-07 02:03:06 -0500

    add inputs for contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,13 +8,63 @@ interface ContactProps {
 
 const Contact = ({ heading, subheading }: ContactProps) => {
   return (
-    <PaddedSection>
-      <MaxWidthContainer className="text-center flex justify-center py-20">
+    <PaddedSection className="bg-gray">
+      <MaxWidthContainer className="text-center flex flex-col items-center py-20">
         <div className="flex flex-col">
           <h3>{heading}</h3>
           <h2>{subheading}</h2>
         </div>
-        <form></form>
+        <form>
+          <div>
+            <input
+              type="text"
+              id="first_name"
+              name="first_name"
+              placeholder="First Name *"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              id="last_name"
+              name="last_name"
+              placeholder="Last Name *"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email *"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="tel"
+              id="phone_number"
+              name="phone_number"
+              placeholder="Phone *"
+              pattern="[0-9]{10}"
+              title="Should only contain ten numbers (no spaces or area code)"
+              required
+            />
+          </div>
+          <div>
+            <textarea
+              className="resize-none"
+              name="comment"
+              id="comment"
+              cols={30}
+              rows={10}
+              placeholder="Comment *"
+              required
+            ></textarea>
+          </div>
+        </form>
       </MaxWidthContainer>
     </PaddedSection>
   );
